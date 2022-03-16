9e4ddf7a87182644097c55230d20f4c7cffd5a9a
Erwin <xfsy_2015@163.com>
2022-03-17 00:43:12 +0800

    first commit

---
+++ src/main/java/com/fengwenyi/springbootactiviti7demo/SpringBootActiviti7DemoApplication.java
@@ -0,0 +1,13 @@
+package com.fengwenyi.springbootactiviti7demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootActiviti7DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootActiviti7DemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fengwenyi/springbootactiviti7demo/SpringBootActiviti7DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fengwenyi.springbootactiviti7demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootActiviti7DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
